07f2b6f5a86696d086243412371a2339d848edc4
brenddamachado <brenda16machado.bm@gmail.com>
2023-09-05 20:01:43 -0300

    atualização final

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 var optionsToDisable = document.querySelectorAll('option[value="saab"]');
 optionsToDisable.forEach(function (option) {
     option.disabled = true;
-});// Desative as opções com valor "saab" e deixe cinza
+});//deixar cinza
 
-// Obtenha uma referência ao botão
+
 const botao = document.getElementById("abrirPagina");
 
-// Adicione um ouvinte de evento de clique ao botão
 botao.addEventListener("click", function() {
-    // Redirecione para a outra página
-    window.location.href = "index.html"; // Substitua "outra_pagina.html" pela URL da página que deseja abrir
+    // a outra página
+    window.location.href = "index.html"; 
 });
