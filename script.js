9d3a058c918a54d9cfd149b7ec2e5fe39eaf2617
brenddamachado <brenda16machado.bm@gmail.com>
2023-09-05 14:03:56 -0300

    porxima page

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 var optionsToDisable = document.querySelectorAll('option[value="saab"]');
 optionsToDisable.forEach(function (option) {
     option.disabled = true;
+});// Desative as opções com valor "saab" e deixe cinza
+
+// Obtenha uma referência ao botão
+const botao = document.getElementById("abrirPagina");
+
+// Adicione um ouvinte de evento de clique ao botão
+botao.addEventListener("click", function() {
+    // Redirecione para a outra página
+    window.location.href = "index.html"; // Substitua "outra_pagina.html" pela URL da página que deseja abrir
 });
